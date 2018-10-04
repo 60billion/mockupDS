3ecf320b38be7d87251344d3005eb930884d9d8f
Allreview <60billion@allrvw.com>
2018-10-04 18:19:22 +0900

    register3

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ app.post('/register',function(req,res){
         var params = [name,email,hash,salt];
         var userInfo = {email:email,password:password};
         conn.query(sql,params,function(err,rows,fields){
-            console.log("success to register: " + email);
+            console.log("success to register: " + rows);
             jwk.sign(userInfo,"secretkey",function(err,token){
                 if(err){
                     console.log("Couldn't register and give token to "+email);
