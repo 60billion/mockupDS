44f1082e4bfcbacfa3b559bd1c8ab91fc1f527d6
Allreview <60billion@allrvw.com>
2018-10-04 13:40:26 +0900

    check git clone

---
+++ server.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var bodyparser = require('body-parser');
+var logger = require('morgan');
+var methodOverride = require('method-override');
+var cors = require('cors');
+var pbkfd2Password = require('pbkdf2-password');
+var hasher = pbkfd2Password();
+var jwk = require('jsonwebtoken');
+//서버구동
+var app = express();
+app.use(logger('dev'));
+app.use(bodyparser.json());
+app.use(methodOverride());
+app.use(cors());
+
+//데이터베이스 접속
+var mysql = require('mysql');
+var conn = mysql.createConnection({
+	host:"testdatabase.c3asktw2nxxm.ap-northeast-2.rds.amazonaws.com",
+    user:"root",
+    password:"11131113",
+    database:"public"
+})
+conn.connect(function(){
+    console.log("connected database!!")
+});
+
+//아마존 접속
+var AWS = require('aws-sdk');
+AWS.config.region = 'ap-northeast-2';
+var s3 = new AWS.S3();
+
+app.get('/',function(req,res){
+	res.send('health test')
+})
