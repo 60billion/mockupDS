2793f3cf2f062ed18f7ff40b97cbf7475ba36cca
Allreview <60billion@allrvw.com>
2018-10-06 16:20:40 +0900

    router test5

--- common/verify.js
+++ common/verify.js
@@ -1,19 +1,22 @@
-module.export = function(req,res,next){
-    const token = req.body.tokens;
-    console.log("verified: "+ token);
-    if(!token || token == undefined){
-        return res.send({
-            login:'login'
-        });
-    }else{
-        jwk.verify(token,'secretkey',(err,code)=>{
-            if(err){
-                console.log("jwk verify err: "+ err);
-            }else{
-                req.code = code;
-                console.log(code);
-                next();
-            }
-        });
+module.export = function(){
+    function verify (req,res,next){
+        const token = req.body.tokens;
+        console.log("verified: "+ token);
+        if(!token || token == undefined){
+            return res.send({
+                login:'login'
+            });
+        }else{
+            jwk.verify(token,'secretkey',(err,code)=>{
+                if(err){
+                    console.log("jwk verify err: "+ err);
+                }else{
+                    req.code = code;
+                    console.log(code);
+                    next();
+                }
+            });
+        }
     }
+    
 }
