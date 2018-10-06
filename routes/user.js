159be42958dc4e73b6d4ab8397bae6c7d57b5d14
Allreview <60billion@allrvw.com>
2018-10-06 15:54:17 +0900

    router test4

--- routes/user.js
+++ routes/user.js
@@ -1,25 +1,25 @@
 module.exports = function(app){
     var express = require('express');
-    var bodyparser = require('body-parser');
-    var logger = require('morgan');
-    var methodOverride = require('method-override');
-    var cors = require('cors');
-    var pbkfd2Password = require('pbkdf2-password');
-    var hasher = pbkfd2Password();
-    var jwk = require('jsonwebtoken');
+    // var bodyparser = require('body-parser');
+    // var logger = require('morgan');
+    // var methodOverride = require('method-override');
+    // var cors = require('cors');
+    // var pbkfd2Password = require('pbkdf2-password');
+    // var hasher = pbkfd2Password();
+    // var jwk = require('jsonwebtoken');
 
-    app.use(logger('dev'));
-    app.use(bodyparser.json());
-    app.use(methodOverride());
-    app.use(cors());
+    // app.use(logger('dev'));
+    // app.use(bodyparser.json());
+    // app.use(methodOverride());
+    // app.use(cors());
 
-    var mysql = require('mysql');
-    var conn = mysql.createConnection({
-        host:"testdatabase.c3asktw2nxxm.ap-northeast-2.rds.amazonaws.com",
-        user:"root",
-        password:"11131113",
-        database:"mockup"
-    });
+    // var mysql = require('mysql');
+    // var conn = mysql.createConnection({
+    //     host:"testdatabase.c3asktw2nxxm.ap-northeast-2.rds.amazonaws.com",
+    //     user:"root",
+    //     password:"11131113",
+    //     database:"mockup"
+    // });
 
     var router = express.Router();
 
