5246aef536cf7d62c1892bcb30dd00fe0e85d54f
Allreview <60billion@allrvw.com>
2018-10-06 15:49:11 +0900

    router test2

--- routes/user.js
+++ routes/user.js
@@ -119,6 +119,26 @@ module.exports = function(app){
             }
         });
     })
+
+    function verify (req,res,next){
+        const token = req.body.tokens;
+        console.log("verified: "+ token);
+        if(!token || token == undefined){
+            return res.send({
+                login:'login'
+            });
+        }else{
+            jwk.verify(token,'secretkey',(err,code)=>{
+                if(err){
+                    console.log("jwk verify err: "+ err);
+                }else{
+                    req.code = code;
+                    console.log(code);
+                    next();
+                }
+            });
+        }
+    }
     
     return router;
 }
