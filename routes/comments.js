69d0963a8fa12796f816278a521e757082159a1c
Allreview <60billion@allrvw.com>
2018-10-21 11:11:36 +0900

    comments delete

--- routes/comments.js
+++ routes/comments.js
@@ -192,11 +192,24 @@ module.exports = function(app){
         var comment = "삭제된 댓글입니다.";
         var category = "삭제";
         var sql = `update ${type} set comment = "${comment}", category = "${category}" where id = ${id}; `;
+        var sql1 = `select * from answ where questId = "${id}";`;
+        var sql2 = `delete from quset where id = ${id};`;
         console.log(sql);
-        conn.query(sql,function(err,rows,fields){
-            console.log("delQuest");
-            if(err) console.log(err);
-            res.send({result:"result"});
+        console.log(sql1);
+        conn.query(sql1,function(err,rows,fields){
+            if(rows[0] == undefined){
+                conn.query(sql2,function(err,rows,fields){
+                    if(err) console.log(err);
+                    console.log("delQuest: deleted all the row.");
+                    res.send({result:"result"});
+                });
+            }else{
+                conn.query(sql,function(err,rows,fields){
+                    if(err) console.log(err);
+                    console.log("delQuest");
+                    res.send({result:"result"});
+                });
+            }
         });
     });
     router.post('/editAnsw',function(req,res){
